43a60798b74f30ee4b1042c512ca36ecc835a081
SDeepesh <63053913+SDeepesh@users.noreply.github.com>
2020-07-04 19:10:47 +0530

    Update todos.js

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
-
 const state = {
   todos: []
 };
-
 const getters = {
   allTodos: state => state.todos
 };
-
 const actions = {
+
+// Fetch data
   async fetchTodos({ commit }) {
     const response = await axios.get(
       'https://jsonplaceholder.typicode.com/todos'
@@ -16,6 +15,8 @@ const actions = {
 
     commit('setTodos', response.data);
   },
+
+//   Add Data to list
   async addTodo({ commit }, title) {
     const response = await axios.post(
       'https://jsonplaceholder.typicode.com/todos',
@@ -24,34 +25,36 @@ const actions = {
 
     commit('newTodo', response.data);
   },
+
+// Delete from list
   async deleteTodo({ commit }, id) {
     await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
 
     commit('removeTodo', id);
   },
+
+// Filter list
   async filterTodos({ commit }, e) {
     const limit = parseInt(
       e.target.options[e.target.options.selectedIndex].innerText
     );
-
     const response = await axios.get(
       `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
     );
 
     commit('setTodos', response.data);
   },
+  
+// Update list
   async updateTodo({ commit }, updTodo) {
     const response = await axios.put(
       `https://jsonplaceholder.typicode.com/todos/${updTodo.id}`,
       updTodo
     );
 
-    console.log(response.data);
-
     commit('updateTodo', response.data);
   }
 };
-
 const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
   newTodo: (state, todo) => state.todos.unshift(todo),
@@ -64,7 +67,6 @@ const mutations = {
     }
   }
 };
-
 export default {
   state,
   getters,
